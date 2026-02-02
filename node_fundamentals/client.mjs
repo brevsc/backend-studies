0078424c75713661702764280abb6d3eac34b288
Breno Vasconcelos <theyukidjr@gmail.com>
2026-02-02 12:53:37 -0300

    feat: searchParams, buffer, body concepts

--- node_fundamentals/client.mjs
+++ node_fundamentals/client.mjs
@@ -1,6 +1,13 @@
-const response = await fetch("http://localhost:3000/", {
-  method: "GET",
-});
+const response = await fetch(
+  "http://localhost:3000/products?color=blue&size=X",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username: "tester", password: "testing" }),
+  },
+);
 
 const body = await response.text();
 
--- node_fundamentals/server.mjs
+++ node_fundamentals/server.mjs
@@ -1,18 +1,34 @@
 import { createServer } from "node:http";
 
-const server = createServer((req, res) => {
+// const part1 = Buffer.from("hello ");
+// const part2 = Buffer.from("world");
+// const parts = Buffer.concat([part1, part2]).toString("utf-8");
+// console.log(parts);
+
+const server = createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  // console.log(req.url);
 
-  switch (`${req.method} ${req.url}`) {
+  const url = new URL(req.url, "http://localhost/3000");
+  const color = url.searchParams.get("color");
+  const size = url.searchParams.get("size");
+  // console.log(req.headers);
+  // console.log(req.url);
+  const chunks = [];
+  for await (const chunk of req) {
+    chunks.push(chunk);
+  }
+  const body = Buffer.concat(chunks).toString('utf-8')
+  console.log(JSON.parse(body).password);
+
+  switch (`${req.method} ${url.pathname}`) {
     case "GET /":
       res.statusCode = 200;
       res.end("HOME");
       break;
     case "POST /products":
       res.statusCode = 201;
-      res.end("PRODUCTS");
+      res.end(`PRODUCTS: ${color}, ${size}`);
       break;
     default:
       res.statusCode = 404;
